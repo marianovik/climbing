c1ea372bb0f530ba0a19dd2a19f4a62d6d6c5068
Maria Novik <marianovik3@gmail.com>
2023-01-25 22:05:39 +0100

    fix: add geo obj

---
+++ lib/fixtures/data/geo_objs.py
@@ -0,0 +1,79 @@
+SVN_CITIES = [
+    {"code": "LJU", "name_en": "Ljubljana"},
+    {"code": "MBX", "name_en": "Maribor"},
+    {"code": "KOP", "name_en": "Koper"},
+    {"code": "CJE", "name_en": "Celje"},
+    {"code": "VEE", "name_en": "Velenje"},
+    {"code": "KRK", "name_en": "Kranj"},
+    {"code": "Gorica", "name_en": "Nova"},
+    {"code": "PTU", "name_en": "Ptuj"},
+    {"code": "TBL", "name_en": "Trbovlje"},
+    {"code": "DZL", "name_en": "Domzale"},
+    {"code": "KMK", "name_en": "Kamnik"},
+    {"code": "JES", "name_en": "Jesenice"},
+    {"code": "ISO", "name_en": "Izola"},
+    {"code": "Mesto", "name_en": "Novo"},
+    {"code": "Sobota", "name_en": "Murska"},
+]
+
+
+SVK_CITIES = [
+    {"code": "BTS", "name_en": "Bratislava"},
+    {"code": "KSC", "name_en": "Kosice"},
+    {"code": "POV", "name_en": "Presov"},
+    {"code": "ZIA", "name_en": "Zilina"},
+    {"code": "NIT", "name_en": "Nitra"},
+    {"code": "Bystrica", "name_en": "Banska"},
+    {"code": "TNA", "name_en": "Trnava"},
+    {"code": "MTN", "name_en": "Martin"},
+    {"code": "TAT", "name_en": "Poprad"},
+    {"code": "TEN", "name_en": "Trencin"},
+    {"code": "ZVL", "name_en": "Zvolen"},
+    {"code": "PVZ", "name_en": "Prievidza"},
+    {"code": "MVY", "name_en": "Michalovce"},
+    {"code": "KOM", "name_en": "Komarno"},
+    {"code": "Streda", "name_en": "Dunajska"},
+]
+
+POL_CITIES = [
+    {"code": "PLGDN", "name_en": "Gdansk"},
+    {"code": "PLWAW", "name_en": "Warsaw"},
+    {"code": "PLKRK", "name_en": "Krakow"},
+    {"code": "PLLOD", "name_en": "Lodz"},
+    {"code": "PLWRO", "name_en": "Wroclaw"},
+    {"code": "PLPOZ", "name_en": "Poznan"},
+    {"code": "PLGDY", "name_en": "Gdynia"},
+    {"code": "PLSZZ", "name_en": "Szczecin"},
+    {"code": "PLBZG", "name_en": "Bydgoszcz"},
+    {"code": "PLLUL", "name_en": "Lublin"},
+    {"code": "PLKTW", "name_en": "Katowice"},
+    {"code": "PLBIC", "name_en": "Bialystok"},
+    {"code": "PLOSP", "name_en": "Olsztyn"},
+    {"code": "PLRZE", "name_en": "Rzeszow"},
+    {"code": "PLKIEL", "name_en": "Kielce"},
+]
+ITA_CITIES = [
+    {"code": "ITROM", "name_en": "Rome"},
+    {"code": "ITMIL", "name_en": "Milan"},
+    {"code": "ITNAP", "name_en": "Naples"},
+    {"code": "ITTUR", "name_en": "Turin"},
+    {"code": "ITPMO", "name_en": "Palermo"},
+    {"code": "ITGOA", "name_en": "Genoa"},
+    {"code": "ITBLQ", "name_en": "Bologna"},
+    {"code": "ITFLR", "name_en": "Florence"},
+    {"code": "ITBRI", "name_en": "Bari"},
+    {"code": "ITCTA", "name_en": "Catania"},
+    {"code": "ITVCE", "name_en": "Venice"},
+    {"code": "ITVRN", "name_en": "Verona"},
+    {"code": "ITMSQ", "name_en": "Messina"},
+    {"code": "ITPAD", "name_en": "Padua"},
+    {"code": "ITTRS", "name_en": "Trieste"},
+]
+
+
+COUNTRIES = [
+    {"code": "ITA", "obj_type": "country", "name_en": "Italy", "cities": ITA_CITIES},
+    {"code": "POL", "obj_type": "country", "name_en": "Poland", "cities": POL_CITIES},
+    {"code": "SVK", "obj_type": "country", "name_en": "Slovakia", "cities": SVK_CITIES},
+    {"code": "SVN", "obj_type": "country", "name_en": "Slovenia", "cities": SVN_CITIES},
+]
